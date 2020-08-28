4e4341b0bcac6a9248278964d69335bcd88bccbe
ClementChaumel <ClementChaumel@users.noreply.github.com>
2020-08-28 07:14:06 +0000

    deploy: 932d01ed30f664e5bfc69a874540cdf8f230ae16

---
+++ _next/static/Sty2IMcfLDaYs72PaMYgX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-7526a7d62c07e4e883e1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4f0601949c71cd429f69.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
